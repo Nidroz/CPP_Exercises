0586e9808ef474d59dcab62c38c6d0482e7ad26f
Nadir SIDHOUM <na.sidhoum@gmail.com>
2024-01-19 11:03:55 +0100

    tp1 - ex1 - q4

--- tp1/ex1/main.cpp
+++ tp1/ex1/main.cpp
@@ -3,11 +3,11 @@
 
 int main()
 {
-    std::cout << "Entre ton nom: ";
+    std::cout << "Entre ton nom: "; // affiche le message
 
-    char name[20] = "";
-    std::cin >> std::setw(20) >> name;
-    std::cout << "Bonjour Palluche La Falluche !" << std::endl;
+    char name[20] = ""; // chaine de characères de taille 20
+    std::cin >> std::setw(20) >> name; // demande le 'name' with 20 caracters field
+    std::cout << "Bonjour " << name << " !" << std::endl; // affiche message bonjour...
 
     return 0;
 }
