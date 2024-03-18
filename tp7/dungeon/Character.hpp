518f2d900de3a0e8cecac804fcd63dcbfe9c714e
Nadir SIDHOUM <na.sidhoum@gmail.com>
2024-03-18 17:10:46 +0100

    TP8 - Exercice 2 / Part A & Part B Fait

---
+++ tp7/dungeon/Character.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include "Entity.hpp"
+
+class Character : public Entity {
+    public:
+        Character(int x, int y)
+            : Entity(x, y)
+        {}
+
+        char get_representation() const override { return 'O'; }
+};
