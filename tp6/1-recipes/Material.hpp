995a10930178dfdcafeacdfb42e1c955f4d47aac
Nadir SIDHOUM <na.sidhoum@gmail.com>
2024-03-18 14:40:09 +0100

    TP6 - Part A & Part B Done !

--- tp6/1-recipes/Material.hpp
+++ tp6/1-recipes/Material.hpp
@@ -1,9 +1,28 @@
 #pragma once
 
 #include <ostream>
+#include <string>
+#include <iostream>
 
 class Material
 {
-    // Affiche le nom d'un materiau.
-    friend std::ostream& operator<<(std::ostream& stream, const Material& material) { return stream; }
+    public:
+        Material(const std::string& name)
+            : _name { name }
+        {
+            std::cout << _name << " was created" << std::endl;
+        }
+
+        ~Material() {
+            std::cout << _name << " was destroyed" << std::endl;
+        }
+
+        // Affiche le nom d'un materiau.
+        friend std::ostream& operator<<(std::ostream& stream, const Material& material) { 
+            return stream << material._name; 
+        }
+
+
+    private:
+        std::string _name;
 };
--- tp6/1-recipes/ProgramData.cpp
+++ tp6/1-recipes/ProgramData.cpp
@@ -2,10 +2,16 @@
 
 
 void ProgramData::add_material(std::string name)
-{}
+{
+    _inventory.emplace_back(std::make_unique<Material>( std::move(name) ));
+}
 
 void ProgramData::get_materials(std::vector<const Material*>& materials) const
-{}
+{
+    for (const auto& material : _inventory) {
+        materials.emplace_back(material.get());
+    }
+}
 
 void ProgramData::register_recipe(std::vector<std::string> materials, std::vector<std::string> products)
 {}
