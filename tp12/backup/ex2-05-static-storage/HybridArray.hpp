39721b327a818446a406ec7d457c4ab3e1cce1ce
Nadir SIDHOUM <na.sidhoum@gmail.com>
2024-04-18 13:12:05 +0200

    TP12 - Exercice 1 & Exercice 2 Done !

---
+++ tp12/backup/ex2-05-static-storage/HybridArray.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <array>
+
+template<typename T, size_t Size>
+class HybridArray {
+    public:
+        HybridArray() = default;
+
+        constexpr static size_t static_size() {
+            return Size;
+        }
+
+        T& push_back(T element) {
+            if (_nbElems >= _array.size()) {
+                if (!copyDynamic) {
+                    for (int i = 0; i < _array.size(); i++) {
+                        _vector.emplace_back(std::move(_array.at(i)));
+                    }
+                    copyDynamic = true;
+                }
+                return _vector.emplace_back(std::move(element));
+            }
+            return _array[_nbElems++] = element;
+        }
+
+        size_t size() const {
+            return _nbElems;
+        }
+
+        T& operator[](size_t index) {
+            if (copyDynamic) {
+                return _vector[index];
+            }
+            return _array[index];
+        }
+
+        const T& operator[](size_t index) const {
+            if (copyDynamic) {
+                return _vector[index];
+            }
+            return _array[index];
+        }
+
+    private:
+        std::array<T, Size> _array;
+        std::vector<T> _vector {};
+        size_t _nbElems = 0;
+        bool copyDynamic = false;
+};
